77bc169b9079d3a1eb119a279e762857dfd7efa5
self.mismatch <self.mismatch@gmail.com>
2023-01-22 19:13:30 +0300

    feat: add faq project

---
+++ projects/11_faq/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const list = document.querySelector('ul');
+
+init();
+
+function init() {
+  list.addEventListener('click', onListClick);
+}
+
+function onListClick(evt) {
+  const item = evt.target.closest('li');
+
+  if (!item) {
+    return;
+  }
+
+  item.classList.toggle('expand');
+}
