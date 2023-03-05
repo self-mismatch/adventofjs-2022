ba3cd1faec686d6f5755128d3f991759df68e820
self.mismatch <self.mismatch@gmail.com>
2023-03-05 19:08:41 +0300

    feat: add custom modal project

---
+++ projects/13_custom-modal/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const modalOpenButton = document.querySelector('.modal-open');
+const modalCloseButton = document.querySelector('.modal-close');
+const overlay = document.querySelector('.overlay');
+const modal = overlay.querySelector('.modal');
+
+let isModalOpen = false;
+
+init();
+
+function init() {
+  modalOpenButton.addEventListener('click', onModalOpenButtonClick);
+}
+
+function showModal() {
+  overlay.classList.add('overlay--show');
+  modal.classList.add('modal--show');
+
+  modalCloseButton.addEventListener('click', onModalCloseButtonClick);
+}
+
+function hideModal() {
+  overlay.classList.remove('overlay--show');
+  modal.classList.remove('modal--show');
+
+  modalCloseButton.removeEventListener('click', onModalCloseButtonClick);
+}
+
+function onModalOpenButtonClick() {
+  showModal();
+}
+
+function onModalCloseButtonClick() {
+  hideModal();
+}
